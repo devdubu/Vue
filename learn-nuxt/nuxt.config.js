35d40e90aebbc7b851222bea88283960085ad1bc
진민 <minjin@hansol.com>
2024-01-08 17:12:13 +0900

    섹션 4 완료

--- learn-nuxt/nuxt.config.js
+++ learn-nuxt/nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/css/reset.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -33,7 +33,9 @@ export default {
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: [({ isLegacy }) => isLegacy && 'axios']
+  },
 
   // server setting
   server:{
--- learn-nuxt/pages/index.vue
+++ learn-nuxt/pages/index.vue
@@ -1,12 +1,104 @@
+import axios from 'axios';
 <template>
-  <div>
-    <h1>홈 페이지</h1>
-    <p>넥스트 시작하기</p>
+  <div class="app">
+    <SearchInput v-model="searchKeyword" @search="searchProducts"/>
+    <main>
+      <ul>
+        <li v-for="product in products" :key="product.id" class="item flex" @click="moveToDetailPage(product.id)">
+          <img class="product-image" :src="product.imageUrl" :alt="product.name">
+          <p>{{ product.name }}</p>
+          <span>{{ product.price }}</span>
+        </li>
+      </ul>    
+    </main>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios'
+import SearchInput from '~/components/SearchInput.vue';
+import { fetchProductsByKeyword } from '~/api';
+
 export default {
-  name: 'IndexPage',
+  components:{
+    SearchInput,
+  },
+    async asyncData(){
+  
+        const response = await axios.get('http://localhost:3000/products');
+        console.log(response)
+        const products = response.data.map((item)=>({
+          ...item,
+          imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+        }))
+        return { products }
+        
+    },
+  data() {
+    return {
+      searchKeyword: '',
+    }
+  },
+    methods:{
+      moveToDetailPage(id){
+        console.log(id)
+        this.$router.push(`detail/${id}`)
+      },
+      updateSearchKeyword(keyword){
+        this.searchKeyword = keyword
+      },
+      async searchProducts(){
+        const response = await fetchProductsByKeyword(this.searchKeyword)
+        console.log(response.data)
+        this.products = response.data.map((item)=>({
+          ...item,
+          imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+        }))
+      }
+    }
+    // data(){
+    //     return{
+    //         products: [],
+    //     }
+    // },
+    
+    // async created(){
+
+    // }
 }
 </script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
